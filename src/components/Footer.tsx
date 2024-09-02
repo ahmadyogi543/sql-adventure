875bdc616d756f23d39bce2a9a6a7a378eaf1362
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-09-02 23:56:39 +0800

    update: change font (again) and fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer className="d-flex justify-content-center align-items-center py-3">
-      <div className="d-flex align-items-center">
+      <div className="d-flex align-items-center text-secondary small">
         <span className="fw-bold me-2">SQL Adventure</span>
-        <span className="text-muted">© 2024 PDWM Pilkom</span>
+        <span className="opacity-75">© 2024 PDWM Pilkom</span>
       </div>
     </footer>
   );
